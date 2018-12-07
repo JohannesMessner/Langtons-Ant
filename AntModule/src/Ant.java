0b4cfdda7c7d673540f49305f5ca9dfbfaaa055f
Johannes Messner <messnerjo@gmail.com>
2018-12-07 13:45:21 +0100

    Implementes Enum Direction that can indicate a direction the ant is facing and can turn left or turn right

--- AntModule/src/Ant.java
+++ AntModule/src/Ant.java
@@ -2,6 +2,10 @@ public class Ant {
 
   private Direction orientation;
 
+  public Ant(){
+    this.orientation = Direction.LEFT;
+  }
+
   public Direction getOrientation() {
     return orientation;
   }
--- AntModule/src/Direction.java
+++ AntModule/src/Direction.java
@@ -1,2 +1,24 @@
 public enum Direction {
+  UP{
+    @Override
+    public Direction turnLeft(){
+      return values()[values().length - 1];
+    }
+  },
+  RIGHT,
+  DOWN,
+  LEFT{
+    @Override
+    public Direction turnRight(){
+      return values()[0];
+    }
+  };
+
+  public Direction turnRight(){
+    return values()[ordinal() + 1];
+  }
+
+  public Direction turnLeft(){
+    return values()[ordinal() - 1];
+  }
 }
