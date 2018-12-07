f8cdb4281c29118f681dffb6941b4f4cf800e68e
Johannes Messner <messnerjo@gmail.com>
2018-12-07 13:36:53 +0100

    Created basic classes and Interfaces

---
+++ AntModule/src/Ant.java
@@ -0,0 +1,8 @@
+public class Ant {
+
+  private Direction orientation;
+
+  public Direction getOrientation() {
+    return orientation;
+  }
+}
---
+++ AntModule/src/Cell.java
@@ -0,0 +1,3 @@
+public interface Cell {
+  public State getState();
+}
