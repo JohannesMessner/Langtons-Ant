f50ea3771e77b8a13bcb677e6fe02c1228de73a3
Johannes Messner <messnerjo@gmail.com>
2018-12-08 11:52:55 +0100

    modified Ant to implement the singletorn design pattern. Also it now holds a Coordinate

--- AntModule/src/Ant.java
+++ AntModule/src/Ant.java
@@ -1,16 +1,27 @@
 public class Ant {
 
+  private static final Ant ant = new Ant();
   private Direction orientation;
+  private Coordinate cor;
 
-  public Ant(){
+  private Ant() {
     this.orientation = Direction.LEFT;
+    this.cor = new Coordinate(0, 0);
   }
 
-  public void turnLeft(){
+  public void reposition(int x, int y) {
+    this.cor = new Coordinate(x, y);
+  }
+
+  public static Ant getInstance() {
+    return ant;
+  }
+
+  public void turnLeft() {
     orientation = orientation.turnLeft();
   }
 
-  public void turnRight(){
+  public void turnRight() {
     orientation = orientation.turnRight();
   }
 
