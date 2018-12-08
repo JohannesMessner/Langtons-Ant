6698c301cf9ad1eca930dc057d801ce7df2f71fe
Johannes Messner <messnerjo@gmail.com>
2018-12-08 11:30:33 +0100

    Implemented Coordinate-class with equals() and hashCode()

---
+++ AntModule/src/AntGrid.java
@@ -0,0 +1,73 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AntGrid implements Grid {
+
+  HashMap<Coordinate, Cell> PlayingField;
+
+  @Override
+  public void setAnt(Ant object, int col, int row) {
+
+  }
+
+  @Override
+  public Map<Coordinate, Ant> getAnts() {
+    return null;
+  }
+
+  @Override
+  public void clearAnts() {
+
+  }
+
+  @Override
+  public void performStep() {
+
+  }
+
+  @Override
+  public void performStep(int number) {
+
+  }
+
+  @Override
+  public void reset(int number) {
+
+  }
+
+  @Override
+  public int getWidth() {
+    return 0;
+  }
+
+  @Override
+  public int getHeight() {
+    return 0;
+  }
+
+  @Override
+  public List getColumn(int i) {
+    return null;
+  }
+
+  @Override
+  public List getRow(int j) {
+    return null;
+  }
+
+  @Override
+  public void resize(int cols, int rows) {
+
+  }
+
+  @Override
+  public void clear() {
+
+  }
+
+  @Override
+  public int getStepCount() {
+    return 0;
+  }
+}
--- AntModule/src/Coordinate.java
+++ AntModule/src/Coordinate.java
@@ -1,2 +1,35 @@
+import java.util.HashMap;
+import java.util.Objects;
+
 public class Coordinate {
+
+  private int x;
+  private int y;
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public Coordinate(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public int hashCode(){
+    return Objects.hash(x,y);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Coordinate)){
+      return false;
+    }
+    Coordinate c = (Coordinate) obj;
+    return c.getX() == this.getX() && c.getY() == this.getY();
+  }
 }
